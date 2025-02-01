1b4f489a91c43274e2965009c566659b54fa85bd
Sushma1707 <sbujula@charlotte.edu>
2025-02-01 00:50:42 -0500

    Added CORS

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-// Budget API
+//Budget API 
 
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+const budget = require("./data.json");
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+app.get("/hello", (req,res)=>{
+    res.send("Hello World.");
+})
 
-
-app.get('/budget', (req, res) => {
+app.get("/budget", (req,res)=>{
     res.json(budget);
-});
+})
 
-app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
-});
+app.listen(port, ()=>{
+    console.log("Api Served at http://localhost:"+port);
+})
