915ad1228287d03b36a0a0a73d9a6767d1d91d78
Sushma1707 <sbujula@charlotte.edu>
2025-02-05 00:13:26 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
 //Budget API 
 
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat Out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-   }
-]
-};
-
-app.get("/hello", (req,res)=>{ 
-    res.send("Hello World.");
-});
-
 app.get("/budget", (req, res) => {
-    res.json({ myBudget: budget });
+    fs.readFile("budget-data.json", "utf8", (err, data) => {
+        if (err) {
+            console.error("Error reading JSON file:", err);
+            res.status(500).send("Server Error");
+            return;
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-}) 
+});
