affee20670d266a97a44d55d67276c619b4a691c
sushmabujula <bujulasushmasri@gmail.com>
2025-02-16 17:41:33 -0500

    Saved local changes before pull

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,34 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import {HomepageComponent} from './homepage/homepage.component'
+import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { P404Component } from './p404/p404.component';
-import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  {
-    path:"",
-    component: HomepageComponent,
-    pathMatch: 'full'
-  },
-  {
-    path:"about",
-    component: AboutComponent
-  },
-  {
-    path:"login",
-    component: LoginComponent
-  },
-  {
-    path:"contact",
-    component: ContactComponent
-  },
-  {
-    path:"**",
-    component: P404Component
-  }
+  { path: '', component: HomepageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: P404Component }  // Catch-all for 404
 ];
 
 @NgModule({
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,10 +1,29 @@
-import { DataService } from '../services/data.service';
+import * as d3 from 'd3';
+ngOnInit(): void {
+  this.createChart();
+}
+
+private createChart(): void {
+  const svg = d3.select('svg')
+    .attr('width', 500)
+    .attr('height', 300);
+
+  svg.append('circle')
+    .attr('cx', 150)
+    .attr('cy', 150)
+    .attr('r', 50)
+    .style('fill', 'blue');
+}
 
 constructor(private dataService: DataService) {}
 
 ngOnInit(): void {
   this.dataService.getData().subscribe((data) => {
-    this.createChart(data);
+    this.renderChart(data);
   });
+}
+
+private renderChart(data: any[]): void {
+  // Use D3.js to visualize `data`
 }
 
